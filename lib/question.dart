b824a72c8dd7d88c2d4d3fbd889f0ce6add5b13c
Solomon APPIER-SIGN <braasig@gmail.com>
2020-01-31 06:15:40 +0000

    added class constructor

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Question {
+  String questionText;
+  bool answer;
+
+  Question(String question, bool answer) {
+    question = questionText;
+    answer = this.answer;
+  }
+}
